8734f694b13232577131ad73ed956046f0ad6f45
ashlyngalloway03-max <ashlyngalloway03@gmail.com>
2025-09-23 14:16:57 +0000

    Update

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -17,10 +17,10 @@ $(function () {
     }
 
     // Create walls - do not delete or modify this code
-    createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
-    createPlatform(-50, -50, 50, canvas.height + 500); // left wall
-    createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
+    createPlatform(-50, -40, canvas.width + 100, 50, "blue"); // top wall
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "blue"); // bottom wall
+    createPlatform(-40, -50, 50, canvas.height + 500, "blue"); // left wall
+    createPlatform(canvas.width,-5, -50, 50, canvas.height + 100, "blue"); // right wall
 
     //////////////////////////////////
     // ONLY CHANGE BELOW THIS POINT //
@@ -32,23 +32,25 @@ $(function () {
 
     // TODO 2 - Create Platforms
 //createPlatform(x, y, width, height)
-createPlatform(1100,675, 100,10, "dark blue")
- createPlatform(900, 600, 100, 10,"white")
- createPlatform(1200,500, 100, 10,"light blue")
- createPlatform(900, 400, 85, 10, "black")
- createPlatform(800, 400, 75, 200, "dark blue")
- createPlatform(800,300,100,10, "light blue")
- createPlatform(500, 400, 150, 20,"white")
+createPlatform(600,650, 100,10, " blue")
+ createPlatform(800, 570, 100, 10,"white")
+ createPlatform(1000,470, 100, 10," blue")
+ createPlatform(970, 250, 100, 10, "black")
+ createPlatform(700, 300, 100, 10, " blue")
+ createPlatform(1200,350,200,10, "black")
+ createPlatform(500, 200, 100, 10,"white")
+ createPlatform(300, 100, 100, 10, "blue")
+ createPlatform(200, 300, 100, 100, "white")
     // TODO 3 - Create Collectables
 
 
 
     
     // TODO 4 - Create Cannons
+//createCannon(wallLocation, position, timeBetweenShots, width, height)
 
-
-    
-    
+    createCannon("top", 1300, 2000, 50,50)
+    createCannon("left", 200, 2000, 50, 50)
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
