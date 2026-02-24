21254c2e022d474f18771d2a1172b214ac60691e
ashlyngalloway03-max <ashlyngalloway03@gmail.com>
2026-02-24 15:19:31 +0000

    Platformering

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,20 +27,27 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    //createPlatform(xPos, yPos, Width, Height, "Color")
+
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
+     createPlatform(500, 0, 20, 290);
+createPlatform(1350, 400, 50, 50, "red");
 
 
 
     // TODO 3 - Create Collectables
-
+createCollectable("steve", 1350, 50);
+createCollectable("diamond", 200, 170, 0.5, 0.7);
 
 
     
     // TODO 4 - Create Cannons
+//createCannon("top / right / bottom / left", position, Speed);
+createCannon("top", 200, 100);
+createCannon("right", 300, 2000);
 
 
     
