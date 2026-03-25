06eef7998a6ebb2288adf0d201b88712379f4b25
ashlyngalloway03-max <ashlyngalloway03@gmail.com>
2026-03-25 13:58:41 +0000

    update

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,9 +18,9 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
-    createPlatform(-50, -50, 50, canvas.height + 500); // left wall
-    createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
+createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
+createPlatform(-50, -50, 50, canvas.height + 500); // left wall
+createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
     //////////////////////////////////
     // ONLY CHANGE BELOW THIS POINT //
@@ -33,22 +33,25 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-     createPlatform(500, 0, 20, 290);
-createPlatform(1350, 400, 50, 50, "red");
-
-
-
+     createPlatform(300, 650, 100, 25, "white");
+createPlatform(500, 575, 100, 25, "white");
+createPlatform (700, 500, 200, 25, "green")
+createPlatform(775, 270, 50, 150, "dark brown" )
+createPlatform(750, 700, 100, 25, "green")
+createPlatform(1000, 550, 100, 30, "white", 0, canvas.width, 0, 200, 550, 2);
+createPlatform(200, 300, 200, 25, "green")
+createPlatform(500, 200, 300, 25, "white")
     // TODO 3 - Create Collectables
-createCollectable("steve", 1350, 50);
-createCollectable("diamond", 200, 170, 0.5, 0.7);
-
+createCollectable("steve", 800,600 );
+createCollectable("steve", 200, 170 );
+  createCollectable("steve", 650, 150)
 
     
     // TODO 4 - Create Cannons
 //createCannon("top / right / bottom / left", position, Speed);
-createCannon("top", 200, 100);
-createCannon("right", 300, 2000);
-
+createCannon("top", 700, 3000);
+createCannon("right", 450, 3000);
+createCannon("right", 700, 3000)
 
     
     
